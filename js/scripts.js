6a0108c8c24109dc61dd7d44005d8b155fae05bf
Ebisu-cmd <gabejardio@gmail.com>
2022-03-22 13:29:31 -0500

    Added pokemonList array
    Array contains three Pokemon objects with properties: name, height, type.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'] },
+    { name: 'Cleffa', height: 0.3, type: ['Fairy'] },
+    { name: 'Giratina', height: 4.5, type: ['Dragon', 'Ghost'] }
+];

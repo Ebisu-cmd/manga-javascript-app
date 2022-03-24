4aeaa692ce7e91b63a151796256d22f7b1611031
Ebisu-cmd <gabejardio@gmail.com>
2022-03-23 20:12:13 -0500

    Pokemon is now listed out in browser
    For loop goes through pokemonList array and displays Pokemon name and height to browser. Conditional is also made in loop that checks if its a big pokemon and displays text if so.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+
+// My array of pokemon
 let pokemonList = [
     { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'] },
-    { name: 'Cleffa', height: 0.3, type: ['Fairy'] },
-    { name: 'Giratina', height: 4.5, type: ['Dragon', 'Ghost'] }
+    { name: 'Giratina', height: 4.5, type: ['Dragon', 'Ghost'] },
+    { name: 'Cleffa', height: 0.3, type: ['Fairy'] } 
 ];
+
+
+// Loop that displays pokemons name and height and checks if its a big pokemon with a conditional
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ` (height: ${pokemonList[i].height})`);
+    if (pokemonList[i].height > 3.0) {
+        document.write(` - Wow, that's big!<br>`);
+    }
+    else {
+        document.write('<br>');
+    }
+}

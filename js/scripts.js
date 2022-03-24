8af28bb9e2db949787fa24c69c6566cf6f0bdf60
Ebisu-cmd <gabejardio@gmail.com>
2022-03-24 14:23:14 -0500

    Use forEach instead of for to display pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,13 @@ let pokemonList = [
     { name: 'Cleffa', height: 0.3, type: ['Fairy'] } 
 ];
 
-
 // Loop that displays pokemons name and height and checks if its a big pokemon with a conditional
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})`)
-    if (pokemonList[i].height > 3.0) {
+pokemonList.forEach(function(pokemon) {
+    document.write(`<p>${pokemon.name} (height: ${pokemon.height})`);
+    if(pokemon.height > 3.0) {
         document.write(` - Wow, that's big!</p>`);
     }
     else {
         document.write('</p>');
     }
-}
+});

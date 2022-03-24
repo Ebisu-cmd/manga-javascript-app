b2d55e0561b294f9ef3e037781a3a6a91a412962
Ebisu-cmd <gabejardio@gmail.com>
2022-03-24 14:45:55 -0500

    Added IIFE for pokemonList array
    -Added two return functions for my IIFE: getAll( ) and add( )
    
    -Also redid forEach loop to use IIFE in order to print out pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
+//IIFE for global data/states
+let pokemonRepository = (function () {
+    
+    //My array of Pokemon
+    let pokemonList = [
+        { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'] },
+        { name: 'Giratina', height: 4.5, type: ['Dragon', 'Ghost'] },
+        { name: 'Cleffa', height: 0.3, type: ['Fairy'] } 
+    ];
 
-// My array of pokemon
-let pokemonList = [
-    { name: 'Gengar', height: 1.5, type: ['Ghost', 'Poison'] },
-    { name: 'Giratina', height: 4.5, type: ['Dragon', 'Ghost'] },
-    { name: 'Cleffa', height: 0.3, type: ['Fairy'] } 
-];
+    // --- My public functions --- 
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+    //Return my array of pokemon
+    function getAll() {
+        return pokemonList;
+    }
+
+    //Add pokemon to pokemonList array
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+})();
+
+
 
 // Loop that displays pokemons name and height and checks if its a big pokemon with a conditional
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(`<p>${pokemon.name} (height: ${pokemon.height})`);
     if(pokemon.height > 3.0) {
         document.write(` - Wow, that's big!</p>`);

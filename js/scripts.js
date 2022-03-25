1d93ceee7928278880f7d74ef3d552862661f954
Ebisu-cmd <gabejardio@gmail.com>
2022-03-24 19:57:36 -0500

    Made addv function for pokemon repository
    addv function verifies that data you want to add is correct

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,8 @@ let pokemonRepository = (function () {
     // --- My public functions --- 
     return {
         getAll: getAll,
-        add: add
+        add: add,  //will comment this out later because addv is the same function but with data verification
+        addv: addv,
     };
 
     //Return my array of pokemon
@@ -24,9 +25,81 @@ let pokemonRepository = (function () {
         pokemonList.push(item);
     }
 
+    //Verifies added pokemon object: adds pokemon to repository if valid / alerts user if pokemon object is invalid
+    function addv(item) {
+        //check if the item is an object
+        if(typeof item === 'object') {
+            //check if object has correct number of keys
+            if(Object.keys(item).length === 3){
+                //check if the object has the correct keys and correct data types for each keys
+                let caseOne = false;
+                let caseTwo = false;
+                let caseThree = false;
+                for(let i = 0; i < 3; i++) {
+                    switch (i) {
+                        case 0:
+                            //check if first key is 'name' and is a String
+                            if(Object.keys(item)[i] === 'name') {
+                                if(typeof item.name === 'string') {
+                                    caseOne = true;
+                                }
+                            }
+                            break;
+
+                        case 1:
+                            //check if second key is 'height' and is a Number
+                            if(Object.keys(item)[i] === 'height') {
+                                if(typeof item.height === 'number') {
+                                    caseTwo = true;
+                                }
+                            }
+                            break;
+
+                        case 2:
+                            let caseThreeOne = false;
+                            let caseThreeTwo = false;
+                            let caseThreeThree = true;
+
+                            //Check if third key is 'type' and is an Array
+                            if(Object.keys(item)[i] === 'type') {
+                                if(Array.isArray(item.type)) {
+                                    caseThreeOne = true;
+                                }
+                            }
+                            
+                            //Check if type array only has one or two types (pokemon can only have 1 or two types)
+                            if(caseThreeOne === true) {
+                                if(item.type.length >= 1 && item.type.length <= 2){
+                                    caseThreeTwo = true;
+                                }
+                            }
+
+                            //Check if third key Array values are Strings
+                            if(caseThreeTwo === true) {
+                                item.type.forEach(function(type) {
+                                    if(typeof type !== 'string'){
+                                        caseThreeThree = false;
+                                    }
+                                });
+                            }
+                            
+                            if(caseThreeOne === true && caseThreeTwo === true && caseThreeThree === true) {
+                                caseThree = true;
+                            }
+                            break;
+                    }
+                }
+                if(caseOne === true && caseTwo === true && caseThree === true) {
+                    add(item);
+                    return;
+                }
+            }
+        }
+        alert('Inputted Pokemon Object is invalid (correct input -> {name: string, height: number, type: array of 1 or 2 strings}');
+    }
+
+    
 })();
-
-
 
 // Loop that displays pokemons name and height and checks if its a big pokemon with a conditional
 pokemonRepository.getAll().forEach(function(pokemon) {

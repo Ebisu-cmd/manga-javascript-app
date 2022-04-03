7d82fa246a53f8450e877f8d821fa051d39f3dee
Ebisu-cmd <gabejardio@gmail.com>
2022-04-02 23:22:58 -0500

    added bootstrap modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonRepository = (function () {
 
     //IIFE variables
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=500';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1126';
     let currentModalPokemon = null;
 
     //Return my array of pokemon
@@ -62,7 +62,7 @@ let pokemonRepository = (function () {
         let button = document.createElement('button');
         
 
-        //set bootstrap classs and text to divs and button
+        //set bootstrap classes and text to divs and button
         rowdiv.classList.add('row', 'align-content-center');
         rowdiv.setAttribute('style', 'height: 100px');
         coldiv.classList.add('col', 'd-flex', 'justify-content-center', 'm-2');
@@ -70,33 +70,34 @@ let pokemonRepository = (function () {
         button.classList.add('btn', 'btn-danger', 'btn-lg', 'w-50');
         button.setAttribute('type', 'button');
         button.setAttribute('style', 'height: 75px');
+        //set button to open bootstrap modal
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#pokemonModal');
 
         //append button to col div, col div to row div, and row div to container
         coldiv.appendChild(button);
         rowdiv.appendChild(coldiv);
         container.appendChild(rowdiv);
 
-    
-
-        //open modal with pokemon details when button is clicked
+        //change modal to have pokemon details when button is clicked
         pokemonButtonListener(button, pokemon);
     }
 
-    //loads Pokemon data from pokemon API and opens modal with data
+    // loads Pokemon data from pokemon API and opens modal with data
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
             showModal(pokemon);
         })
     }
 
-    //add event listener to new pokemon buttton
+    // add event listener to new pokemon buttton
     function pokemonButtonListener(button, pokemon) {
         button.addEventListener('click', function() {
             showDetails(pokemon);
         });
     }
 
-    //load list of pokemon from external pokemon api
+    // load list of pokemon from external pokemon api
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -113,7 +114,7 @@ let pokemonRepository = (function () {
         })
     }
 
-    //uses imageUrl of pokemon object to fetch more pokemon details from API
+    // uses imageUrl of pokemon object to fetch more pokemon details from API
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -129,27 +130,63 @@ let pokemonRepository = (function () {
 
     // opens modal with pokemon information
     function showModal(pokemon) {
-        currentModalPokemon = pokemon;
-        let modalContainer = document.querySelector('#modal-container');
+        // currentModalPokemon = pokemon;
+        // let modalContainer = document.querySelector('#modal-container');
       
-        // clear all existing modal content
-        modalContainer.innerHTML = '';
+        // // clear all existing modal content
+        // modalContainer.innerHTML = '';
       
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.setAttribute('pointer-action', 'none');
+        // let modal = document.createElement('div');
+        // modal.classList.add('modal');
+        // modal.setAttribute('pointer-action', 'none');
       
-        // adds close button for modal with event listener
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'X';
-        closeButtonElement.addEventListener('click', hideModal);
+        // // adds close button for modal with event listener
+        // let closeButtonElement = document.createElement('button');
+        // closeButtonElement.classList.add('modal-close');
+        // closeButtonElement.innerText = 'X';
+        // closeButtonElement.addEventListener('click', hideModal);
         
-        // title as pokemon name
-        let titleElement = document.createElement('h1');
-        titleElement.innerText = pokemon.name;
+        // // title as pokemon name
+        // let titleElement = document.createElement('h1');
+        // titleElement.innerText = pokemon.name;
       
-        // information displayed is sprite, height and types
+        // // information displayed is sprite, height and types
+        // let imageContent = document.createElement('img');
+        // imageContent.setAttribute('src', pokemon.imageUrl);
+        // let heightElement = document.createElement('p');
+        // heightElement.innerText = 'height: ' + pokemon.height;
+        // let typeArray = pokemon.types.map(function (index) {
+        //     return index.type.name;
+        // })
+        // let typeElement = document.createElement('p');
+        // typeElement.innerText = 'type:'
+        // typeArray.forEach(function (type) {
+        //     typeElement.innerText += ' ' + type;
+        // });
+        
+      
+        // modal.appendChild(closeButtonElement);
+        // modal.appendChild(titleElement);
+        // modal.appendChild(imageContent);
+        // modal.appendChild(heightElement);
+        // modal.appendChild(typeElement);
+        // modalContainer.appendChild(modal);
+      
+        // modalContainer.classList.add('is-visible');
+
+        // nodes used for modal
+        let modalTitle = document.querySelector('.modal-title');
+        let modalBody = document.querySelector('.modal-body');
+
+        // erase all existing modal content
+        while (modalBody.firstChild) {
+            modalBody.removeChild(modalBody.firstChild);
+        }
+
+        // append pokemon name to modal title
+        modalTitle.innerText = pokemon.name;
+
+        // get all details about pokemon
         let imageContent = document.createElement('img');
         imageContent.setAttribute('src', pokemon.imageUrl);
         let heightElement = document.createElement('p');
@@ -162,44 +199,13 @@ let pokemonRepository = (function () {
         typeArray.forEach(function (type) {
             typeElement.innerText += ' ' + type;
         });
+
+        // append all pokemon details to modal body
+        modalBody.appendChild(imageContent);
+        modalBody.appendChild(heightElement);
+        modalBody.appendChild(typeElement);
         
-      
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(titleElement);
-        modal.appendChild(imageContent);
-        modal.appendChild(heightElement);
-        modal.appendChild(typeElement);
-        modalContainer.appendChild(modal);
-      
-        modalContainer.classList.add('is-visible');
     }
-
-    // hides modal 
-    function hideModal() {
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.remove('is-visible');
-        currentModalPokemon = null;
-    }
-
-     // closes modal when escape key is pressed
-    let modalContainer = document.querySelector('#modal-container');
-    window.addEventListener('keydown', function (e) {
-        let modalContainer = document.querySelector('#modal-container');
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-            hideModal();  
-        }
-    });
-
-    // closes modal when user clicks outside of modal
-    modalContainer.addEventListener('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal
-        // We only want to close if the user clicks directly on the overlay
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-    });
-
 
     //variables for pointer events
     let startX = null;
@@ -237,8 +243,8 @@ let pokemonRepository = (function () {
     }
 
     // event listeners for swiping between data items
-    modalContainer.addEventListener("pointerdown", handleStart);
-    modalContainer.addEventListener("pointerup", handleEnd);
+    // modalContainer.addEventListener("pointerdown", handleStart);
+    // modalContainer.addEventListener("pointerup", handleEnd);
     
 
 
